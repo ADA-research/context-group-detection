c0461cce933c7b3c1f80cd6b46f508905213fbd6
thomasmaliappis <thomasmaliappis@gmail.com>
2023-04-11 19:10:35 +0200

    merging groups with common agents

--- datasets/excel_report.py
+++ datasets/excel_report.py
@@ -6,6 +6,12 @@ from loader import read_obsmat, read_groups
 
 
 def report(name, stats):
+    '''
+    Generate excel report file with dataset stats.
+    :param name: string for Excel file name
+    :param stats: dictionary of stats for every dataset
+    :return: nothing
+    '''
     workbook = xlsxwriter.Workbook(name)
     worksheet = workbook.add_worksheet('Datasets')
     header_row = 0
@@ -14,8 +20,8 @@ def report(name, stats):
     worksheet.write(header_row, header_column + 1, 'Agents #')
     worksheet.write(header_row, header_column + 2, 'Frames #')
     worksheet.write(header_row, header_column + 3, 'Groups #')
-    worksheet.write(header_row, header_column + 4, 'Agents # in multiple groups')
-    worksheet.write(header_row, header_column + 5, 'Single agent groups #')
+    # worksheet.write(header_row, header_column + 4, 'Agents # in multiple groups')
+    # worksheet.write(header_row, header_column + 5, 'Single agent groups #')
 
     row = header_row + 1
     for key in stats.keys():
@@ -23,14 +29,19 @@ def report(name, stats):
         worksheet.write(row, 1, stats[key]['agents'])
         worksheet.write(row, 2, stats[key]['frames'])
         worksheet.write(row, 3, stats[key]['groups'])
-        worksheet.write(row, 4, stats[key]['multigroup agents'])
-        worksheet.write(row, 5, stats[key]['single agent groups'])
+        # worksheet.write(row, 4, stats[key]['multigroup agents'])
+        # worksheet.write(row, 5, stats[key]['single agent groups'])
         row += 1
 
     workbook.close()
 
 
 def dataset_stats(dataset_path):
+    '''
+    Get stats for specified dataset.
+    :param dataset_path: string of where to find dataset
+    :return: dictionary with stats
+    '''
     df = read_obsmat(dataset_path)
     groups = read_groups(dataset_path)
 
@@ -39,6 +50,16 @@ def dataset_stats(dataset_path):
 
     count_dict = Counter([agent for group in groups for agent in group])
     agents_in_multiple_groups = [key for key, value in count_dict.items() if value > 1]
+    if len(agents_in_multiple_groups) > 0:
+        groups_with_duplicate_agents_indices = \
+            set(i for i, group in enumerate(groups) for agent in agents_in_multiple_groups if agent in group)
+        groups_without_duplicate_agents = \
+            [group for i, group in enumerate(groups) if i not in groups_with_duplicate_agents_indices]
+        groups_with_duplicate_agents = \
+            [group for i, group in enumerate(groups) if i in groups_with_duplicate_agents_indices]
+        groups = \
+            groups_without_duplicate_agents + merge_groups_with_common_agents(agents_in_multiple_groups, groups_with_duplicate_agents)
+
     agents_in_groups = [agent for agent in count_dict.elements()]
     single_groups = agents_num - len(agents_in_groups)
 
@@ -51,6 +72,25 @@ def dataset_stats(dataset_path):
     }
 
 
+def merge_groups_with_common_agents(agents_in_multiple_groups, groups):
+    '''
+    Merge groups with common agents.
+    :param groups: list of lists representing the agent groups
+    :return: list of lists without agents being in multiple groups
+    '''
+    for agent in agents_in_multiple_groups:
+        group_indices = []
+        for i, group in enumerate(groups):
+            if agent in group:
+                group_indices.append(i)
+        groups_to_be_merged = [groups[i] for i in group_indices]
+        merged_group = list(set(agent for group in groups_to_be_merged for agent in group))
+        for c, i in enumerate(group_indices):
+            groups.pop(i - c)
+        groups.append(merged_group)
+    return groups
+
+
 if __name__ == '__main__':
     stats = {
         'eth': dataset_stats('./ETH/seq_eth'),
@@ -60,5 +100,4 @@ if __name__ == '__main__':
         'students03': dataset_stats('./UCY/students03')
     }
 
-    workbook_name = 'datasets.xlsx'
-    report(workbook_name, stats)
+    report('datasets.xlsx', stats)
