5cb4a63b32e26cd3096750d95dd8f180a9b5b3f8
thomasmaliappis <thomasmaliappis@gmail.com>
2023-05-26 14:38:21 +0200

    removed unused files

--- datasets/loader.py
+++ datasets/loader.py
@@ -3,43 +3,6 @@ from collections import Counter
 import numpy as np
 import pandas as pd
 
-from datasets.trajdataset import TrajDataset
-
-
-def load_eth(path, **kwargs):
-    traj_dataset = TrajDataset()
-
-    csv_columns = ["frame_id", "agent_id", "pos_x", "pos_z", "pos_y", "vel_x", "vel_z", "vel_y"]
-    # read from csv => fill traj table
-    raw_dataset = pd.read_csv(path, sep=r"\s+", header=None, names=csv_columns)
-
-    traj_dataset.title = kwargs.get('title', "no_title")
-
-    # copy columns
-    traj_dataset.data[["frame_id", "agent_id",
-                       "pos_x", "pos_y",
-                       "vel_x", "vel_y"
-                       ]] = \
-        raw_dataset[["frame_id", "agent_id",
-                     "pos_x", "pos_y",
-                     "vel_x", "vel_y"
-                     ]]
-
-    traj_dataset.data["scene_id"] = kwargs.get('scene_id', 0)
-    traj_dataset.data["label"] = "pedestrian"
-
-    # post-process
-    fps = kwargs.get('fps', -1)
-    if fps < 0:
-        d_frame = np.diff(pd.unique(raw_dataset["frame_id"]))
-        fps = d_frame[0] * 2.5  # 2.5 is the common annotation fps for all (ETH+UCY) datasets
-
-    sampling_rate = kwargs.get('sampling_rate', 1)
-    use_kalman = kwargs.get('use_kalman', False)
-    traj_dataset.postprocess(fps=fps, sampling_rate=sampling_rate, use_kalman=use_kalman)
-
-    return traj_dataset
-
 
 def read_obsmat(directory):
     '''
@@ -118,8 +81,3 @@ if __name__ == '__main__':
     dataset_path = './UCY/students03'
     eth_df = read_obsmat(dataset_path)
     eth_groups = read_groups(dataset_path)
-
-    eth_traj_dataset = load_eth('./ETH/seq_eth/obsmat.txt')
-    eth_trajs = eth_traj_dataset.get_trajectories()
-    traj_1 = eth_traj_dataset.data.iloc[eth_trajs.groups[0, 1]]  # get trajectory of scene_id 0 and pedestrian_id 1
-    traj_1_v2 = eth_traj_dataset.data[eth_traj_dataset.data['agent_id'] == 1]
