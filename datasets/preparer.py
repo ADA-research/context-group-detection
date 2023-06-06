b41e71e15de0f52d438ba88fd5a9ded1200cdfca
thomasmaliappis <thomasmaliappis@gmail.com>
2023-06-06 16:52:26 +0200

    fixed split into folds

--- datasets/preparer.py
+++ datasets/preparer.py
@@ -11,11 +11,12 @@ import pandas as pd
 import seaborn as sns
 import xlsxwriter
 from matplotlib import pyplot as plt
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import train_test_split
 
 from datasets.loader import read_obsmat, read_groups
 
 random.seed(14)
+np.random.seed(14)
 
 
 def report(name, data):
@@ -104,6 +105,18 @@ def dataset_data(dataset_path):
         'difference': frames_difference,
         'duration': df.loc[df.frame_id.idxmax()]['timestamp'] - df.loc[df.frame_id.idxmin()]['timestamp']
     }
+
+
+def get_group_pairs(groups):
+    """
+    Get pairs of agents that are in same group.
+    :param groups: list of groups
+    :return: list of pairs of agents
+    """
+    pairs = []
+    for group in groups:
+        pairs.extend(list(combinations(group, 2)))
+    return pairs
 
 
 def remove_agents_in_low_number_of_frames(dataframe, agent_ids_to_be_removed):
@@ -357,7 +370,7 @@ def dataset_reformat(dataframe, groups, group_pairs, frame_comb_data, agents_min
     labels = []
     frames = []
     combs_groups = []
-    for frame_comb in frame_comb_data[:10]:
+    for frame_comb in frame_comb_data:
         comb_frames = frame_comb['frames']
         comb_agents = frame_comb['common_agents']
         comb_groups = frame_comb['groups']
@@ -375,33 +388,39 @@ def dataset_reformat(dataframe, groups, group_pairs, frame_comb_data, agents_min
                                                                                               dtype=object)
 
 
-def get_group_pairs(groups):
+def folds_split(frames, folds_num, multi_frame=False):
     """
-    Get pairs of agents that are in same group.
-    :param groups: list of groups
-    :return: list of pairs of agents
+    Split frames based on frame id and split in folds.
+    :param frames: list of frames
+    :param folds_num: number of folds to split frames
+    :param multi_frame: True if scenes include multiple frames, otherwise False
+    :return:
     """
-    pairs = []
-    for group in groups:
-        pairs.extend(list(combinations(group, 2)))
-    return pairs
+    frame_ids = [frame[0] for frame in frames]
+    if multi_frame:
+        frame_values = [list(x) for x in set(tuple(frame_id) for frame_id in frame_ids)]
+    else:
+        frame_values = np.unique(frame_ids)
+
+    fold_size = len(frame_values) // folds_num
+
+    folds_idx = []
+    for i in range(folds_num):
+        start_idx = i * fold_size
+        end_idx = (i + 1) * fold_size
+
+        if i == folds_num - 1:
+            test_fold_frame_values = frame_values[start_idx:]
+        else:
+            test_fold_frame_values = frame_values[start_idx:end_idx]
+        test_fold_idx = [i for i, frame in enumerate(frame_ids) if frame in test_fold_frame_values]
+        train_fold_idx = [i for i, frame in enumerate(frame_ids) if frame not in test_fold_frame_values]
+        folds_idx.append((train_fold_idx, test_fold_idx))
+
+    return folds_idx
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-r', '--report', action="store_true", default=False)
-    parser.add_argument('-p', '--plot', action="store_true", default=False)
-    parser.add_argument('-f', '--frames_num', type=int, default=1)
-    parser.add_argument('-a', '--agents_num', type=int, default=10)
-    parser.add_argument('-ts', '--target_size', type=int, default=100000)
-    parser.add_argument('-d', '--dataset', type=str, default='eth')
-    parser.add_argument('-sf', '--save_folder', type=str, default='./reformatted')
-
-    return parser.parse_args()
-
-
-def train_test_split_frames(frames, multi_frame=False):
+def train_val_split_frames(frames, multi_frame=False):
     """
     Split train, test and val indices.
     :param frames: list of frames
@@ -411,21 +430,12 @@ def train_test_split_frames(frames, multi_frame=False):
     frame_ids = [frame[0] for frame in frames]
     if multi_frame:
         frame_values = [list(x) for x in set(tuple(frame_id) for frame_id in frame_ids)]
-        train, test = train_test_split(frame_values, test_size=0.3, random_state=0)
-        idx_train = [i for i, frame in enumerate(frame_ids) if frame in train]
-        frame_ids_train = [frame[0] for frame in frames[idx_train]]
-        frame_values_train = [list(x) for x in set(tuple(frame_id) for frame_id in frame_ids_train)]
     else:
         frame_values = np.unique(frame_ids)
-        train, test = train_test_split(frame_values, test_size=0.3, random_state=0)
-        idx_train = [i for i, frame in enumerate(frame_ids) if frame in train]
-        frame_ids_train = [frame[0] for frame in frames[idx_train]]
-        frame_values_train = np.unique(frame_ids_train)
-    train, val = train_test_split(frame_values_train, test_size=0.2, random_state=0)
+    train, val = train_test_split(frame_values, test_size=0.3, random_state=0)
     idx_train = [i for i, frame in enumerate(frame_ids) if frame in train]
-    idx_test = [i for i, frame in enumerate(frame_ids) if frame in test]
     idx_val = [i for i, frame in enumerate(frame_ids) if frame in val]
-    return idx_train, idx_test, idx_val
+    return idx_train, idx_val
 
 
 def train_test_split_groups(groups, frames_train, frames_test, frames_val, multi_frame=False):
@@ -461,18 +471,13 @@ def dump(path, data):
         pickle.dump(data, f)
 
 
-# todo split dataset in folds
-def save_folds(save_folder, dataset, frames_num, agents_num, data, labels, frames, groups, multi_frame, folds=5,
+def save_folds(save_folder, dataset, frames_num, agents_num, data, labels, frames, groups, multi_frame, folds_num=5,
                features=4):
-    kf = KFold(n_splits=folds, shuffle=True, random_state=14)
-
     if not multi_frame:
         data = data.reshape((len(data), 1, agents_num, features))
 
-    # todo fix use of train_index and test_index
-    for i, (train_index, test_index) in enumerate(kf.split(data)):
-
-        idx_train, idx_test, idx_val = train_test_split_frames(frames, multi_frame)
+    for i, (idx_train_val, idx_test) in enumerate(folds_split(frames, folds_num, multi_frame)):
+        idx_train, idx_val = train_val_split_frames(frames[idx_train_val], multi_frame)
         groups_train, groups_test, groups_val = \
             train_test_split_groups(groups, frames[idx_train], frames[idx_test], frames[idx_val], multi_frame)
 
@@ -494,19 +499,33 @@ def save_folds(save_folder, dataset, frames_num, agents_num, data, labels, frame
         dump('{}/val.p'.format(path), val)
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-r', '--report', action="store_true", default=False)
+    parser.add_argument('-p', '--plot', action="store_true", default=False)
+    parser.add_argument('-f', '--frames_num', type=int, default=10)
+    parser.add_argument('-a', '--agents_num', type=int, default=10)
+    parser.add_argument('-ts', '--target_size', type=int, default=100000)
+    parser.add_argument('-d', '--dataset', type=str, default='eth')
+    parser.add_argument('-sf', '--save_folder', type=str, default='./reformatted')
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
     start = datetime.now()
-    print('Started...')
+    print('Started at {}'.format(start))
 
     args = get_args()
 
     # create datasets report
     datasets_dict = {
         'eth': dataset_data('./ETH/seq_eth'),
-        # 'hotel': dataset_data('./ETH/seq_hotel'),
-        # 'zara01': dataset_data('./UCY/zara01'),
-        # 'zara02': dataset_data('./UCY/zara02'),
-        # 'students03': dataset_data('./UCY/students03')
+        'hotel': dataset_data('./ETH/seq_hotel'),
+        'zara01': dataset_data('./UCY/zara01'),
+        'zara02': dataset_data('./UCY/zara02'),
+        'students03': dataset_data('./UCY/students03')
     }
     if args.report:
         report('datasets.xlsx', datasets_dict)
@@ -570,6 +589,7 @@ if __name__ == '__main__':
         }
     for dataset in datasets_dict.keys():
         dataset_start = datetime.now()
+        print('Dataset: {}, started at: {}'.format(dataset, dataset_start))
 
         df = datasets_dict[dataset]['df']
         groups = datasets_dict[dataset]['groups']
@@ -591,6 +611,7 @@ if __name__ == '__main__':
                                                                  min_pair_samples=min_samples[dataset],
                                                                  max_pair_samples=max_samples[dataset])
 
+        # save dataset in folds
         save_folds(args.save_folder, dataset, args.frames_num, args.agents_num, data, labels, frames, filtered_groups,
                    multi_frame)
 
