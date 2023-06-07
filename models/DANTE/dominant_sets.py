ab2e708514674d72d2ac257e3d85b6100e6d8932
thomasmaliappis <thomasmaliappis@gmail.com>
2023-06-07 11:31:57 +0200

    added prints to debug dominant sets issue

--- models/DANTE/dominant_sets.py
+++ models/DANTE/dominant_sets.py
@@ -101,11 +101,14 @@ def vector_climb(A, allowed, n_people, original_A, thres=1e-5, eps_threshold=1e-
     x = np.random.uniform(0, 1, n_people)
     x = np.multiply(x, allowed)
     eps = 10
+    counter = 0
     while eps > eps_threshold:
         p = f(x, A)
         x = np.multiply(x, np.dot(A, x)) / np.dot(x, np.dot(A, x))
         n = f(x, A)
         eps = abs(n - p)
+        counter += 1
+    print('vector climb counter: {}'.format(counter))
 
     groups = x > thres
 
@@ -135,6 +138,7 @@ def iterate_climb_learned(predictions, n_people, frames, n_features=None, new=Fa
         A = learned_affinity(predictions, n_people, frames, n_features)
     original_A = A.copy()
     # TODO force this to end if it runs for too long
+    counter = 0
     while np.sum(allowed) > 1:
         A[allowed == False] = 0
         A[:, allowed == False] = 0
@@ -145,6 +149,8 @@ def iterate_climb_learned(predictions, n_people, frames, n_features=None, new=Fa
             break
         groups.append(x)
         allowed = np.multiply(x == False, allowed)
+        counter += 1
+    print('iterate climb counter: {}'.format(counter))
 
     if new:
         return groups, agents_map
