5a8afcf56562bc48558e16c6e75c25dcf3591d13
thomasmaliappis <thomasmaliappis@gmail.com>
2023-06-19 17:28:48 +0200

    updated according to results.txt changes

--- models/collector.py
+++ models/collector.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+import numpy as np
+
 
 def read_results(folder_path):
     file_path = folder_path + '/results.txt'
@@ -12,27 +14,42 @@ def read_results(folder_path):
             'value': float(lines[0][1]),
             'epoch': int(lines[1][1])
         },
-        'best_val_f1_1': {
+        'best_f1_avg': {
             'value': float(lines[2][1]),
             'epoch': int(lines[3][1]),
             'test_f1s': [float(i) for i in lines[4][1:]],
             'precision': [float(i) for i in lines[5][1:]],
             'recall': [float(i) for i in lines[6][1:]]
         },
-        'best_val_f1_2/3': {
+        'best_val_f1_1': {
             'value': float(lines[7][1]),
             'epoch': int(lines[8][1]),
             'test_f1s': [float(i) for i in lines[9][1:]],
             'precision': [float(i) for i in lines[10][1:]],
             'recall': [float(i) for i in lines[11][1:]]
         },
-        'best_val_f1_gmitre': {
+        'best_val_f1_2/3': {
             'value': float(lines[12][1]),
             'epoch': int(lines[13][1]),
             'test_f1s': [float(i) for i in lines[14][1:]],
             'precision': [float(i) for i in lines[15][1:]],
             'recall': [float(i) for i in lines[16][1:]]
-        }
+        },
+        'best_val_f1_gmitre': {
+            'value': float(lines[17][1]),
+            'epoch': int(lines[18][1]),
+            'test_f1s': [float(i) for i in lines[19][1:]],
+            'precision': [float(i) for i in lines[20][1:]],
+            'recall': [float(i) for i in lines[21][1:]]
+        },
+        "train loss": [float(line[0]) for line in lines[23:]],
+        "val loss": [float(line[1]) for line in lines[23:]],
+        "train mse": [float(line[2]) for line in lines[23:]],
+        "val mse": [float(line[3]) for line in lines[23:]],
+        "val 1 f1": [float(line[4]) for line in lines[23:]],
+        "val 2/3 f1": [float(line[5]) for line in lines[23:]],
+        "val gmitre f1": [float(line[6]) for line in lines[23:]],
+        "f1 avg": [float(line[7]) for line in lines[23:]],
     }
 
     return results
@@ -45,10 +62,10 @@ def get_averages(results):
     f1_gmitre_vals = [res['best_val_f1_gmitre']['value'] for res in results]
 
     return {
-        'mse': sum(mse_vals) / len(mse_vals),
-        'f1_1': sum(f1_1_vals) / len(f1_1_vals),
-        'f1_2/3': sum(f1_2_vals) / len(f1_2_vals),
-        'f1_gmitre': sum(f1_gmitre_vals) / len(f1_gmitre_vals)
+        'mse': (np.mean(mse_vals), np.std(mse_vals)),
+        'f1_1': (np.mean(f1_1_vals), np.std(f1_1_vals)),
+        'f1_2/3': (np.mean(f1_2_vals), np.std(f1_2_vals)),
+        'f1_gmitre': (np.mean(f1_gmitre_vals), np.std(f1_gmitre_vals))
     }
 
 
@@ -68,9 +85,11 @@ def collect_results(results_path, dir_name):
 def write_results(results, file_path, dir_name):
     file_path = file_path + '/{}_results.txt'.format(dir_name)
     with open(file_path, "w") as file:
-        file.write('{:<10s} {:<10s} {:<10s} {:<10s}\n'.format('mse', '1 f1', '2/3 f1', 'gmitre f1'))
-        file.write('{:<10.7f} {:<10.7f} {:<10.7f} {:<10.7f}\n'.format(
-                results['mse'], results['f1_1'], results['f1_2/3'], results['f1_gmitre']))
+        file.write('{:<10s} {:<10s} {:<10s} {:<10s} {:<10s}\n'.format('', 'mse', '1 f1', '2/3 f1', 'gmitre f1'))
+        file.write('{:<10s} {:<10.7f} {:<10.7f} {:<10.7f} {:<10.7f}\n'.format(
+            'mean', results['mse'][0], results['f1_1'][0], results['f1_2/3'][0], results['f1_gmitre'][0]))
+        file.write('{:<10s} {:<10.7f} {:<10.7f} {:<10.7f} {:<10.7f}\n'.format(
+            'std', results['mse'][1], results['f1_1'][1], results['f1_2/3'][1], results['f1_gmitre'][1]))
 
 
 def get_args():
